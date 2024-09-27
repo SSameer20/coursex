450304e958291752b31b6e0c6fe20fd47f7ffd44
Sameer Shaik <sameer.kattubadi@gmail.com>
2024-09-27 21:53:31 +0530

    fix : user details api

--- server/index.js
+++ server/index.js
@@ -1,48 +1,39 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-
-const {userRouter} = require('./routes/user')
-const {adminRouter} = require('./routes/admin')
+const { userRouter } = require("./routes/user");
+const { adminRouter } = require("./routes/admin");
 
 const app = express();
 
-
 // Middlewares
-app.use(cors(
-    {
-        origin : ["http://localhost:3000/"],
-        methods : ["POST", "GET"],
-        credentials : true
-    }
-));
+app.use(cors());
 app.use(bodyParser.json());
 
 // Routes
-app.use("/api/v1/user", userRouter)
-app.use("/api/v1/admin", adminRouter)
-
-
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/admin", adminRouter);
 
 app.get("/", (req, res) => {
-    res.json({
-        message: "server is running"
+  res.json({
+    message: "server is running",
+  });
+});
+
+const main = async () => {
+  await mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log("Connected DB");
+      const PORT = process.env.PORT || 8080;
+      app.listen(PORT, () => {
+        console.log(`Server is runing http://localhost:${PORT}`);
+      });
     })
-})
+    .catch((err) => console.log(err));
+};
 
-
-
-
-const main = async() => {
-    await mongoose.connect(process.env.MONGO_URI)
-    .then(console.log("DB connceted"))
-    .catch(err => console.log(err))
-    const PORT = process.env.PORT || 8080
-    app.listen(PORT, () => {
-        console.log(`Server is runing http://localhost:${PORT}`)
-    })
-}
-main()
+main();
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,141 +1,148 @@
-const express = require('express');
-const {Router} = express
-const {UserModel, PurchaseModel, CourseModel} = require('../db')
-const JWT = require('jsonwebtoken')
-const userAuth = require('../auth/userAuth')
-
+const express = require("express");
+const { Router } = express;
+const { UserModel, PurchaseModel, CourseModel } = require("../db");
+const JWT = require("jsonwebtoken");
+const userAuth = require("../auth/userAuth");
 
 const userRouter = Router();
 
-
-
 /*
-    * Status Code Examples
-    *   @successful - 201
-    *   @not found - 404
-    *   @error  - 500
-*/
-
+ * Status Code Examples
+ *   @successful - 201
+ *   @not found - 404
+ *   @error  - 500
+ */
 
 userRouter.post("/login", async (req, res) => {
-    try {
-        console.log(req.body.userDetails)
-        const {email, password} = req.body.userDetails;
+  try {
+    const { email, password } = req.body.userDetails;
 
-        if(!email || !password)  return res.status(400).send({ message: "All fields are required to Login" });
+    if (!email || !password)
+      return res
+        .status(400)
+        .send({ message: "All fields are required to Login" });
 
-        const findUser = await UserModel.findOne({email});
+    const findUser = await UserModel.findOne({ email });
 
-        if(!findUser) return res.status(404).send({ message: "no user found" });
+    if (!findUser) return res.status(404).send({ message: "no user found" });
 
-        let passwordValidation = findUser.password === btoa(password) ? true : false;
+    let passwordValidation =
+      findUser.password === btoa(password) ? true : false;
 
-        if(!passwordValidation) return res.status(404).send({ message: "wrong credentials" });
-        
-        const token = JWT.sign(
-            { userId: findUser._id, email: findUser.email }, 
-            process.env.SECRET_KEY, 
-            { expiresIn: '1h' }
-        );
+    if (!passwordValidation)
+      return res.status(404).send({ message: "wrong credentials" });
 
-        return res.status(201).send({message : "user logged successfully", token});
+    const token = JWT.sign(
+      { userId: findUser._id, email: findUser.email },
+      process.env.SECRET_KEY,
+      { expiresIn: "1h" }
+    );
 
-    } catch (error) {
-        return res.status(500).send({ message: error });
-    }
-})
+    return res.status(201).send({ message: "user logged successfully", token });
+  } catch (error) {
+    return res.status(500).send({ message: error });
+  }
+});
 
 userRouter.post("/register", async (req, res) => {
-    try {
-        const { email, password, firstName, lastName } = req.body.userDetails;
-       
-        if (!email || !password || !firstName || !lastName) {
-            return res.status(404).send({ message: "All fields are required to register" });
-        }
-       
-        const findUser = await UserModel.findOne({ email });
-        if (findUser) {
-            console.log(findUser)
-            return res.status(404).send({ message: "User already exists with the same email" });
-        }
+  try {
+    const { email, password, firstName, lastName } = req.body.userDetails;
 
-      
-        const hashPassword = btoa(password)
-        const user = new UserModel({
-            email,
-            password: hashPassword,
-            firstName,
-            lastName
-        });
-
-        await user.save();
-        console.log(user)
-        return res.status(201).send({ message: "Successfully created user" });
-    } catch (error) {
-        return res.status(500).send({ message: error.message });
+    if (!email || !password || !firstName || !lastName) {
+      return res
+        .status(404)
+        .send({ message: "All fields are required to register" });
     }
+
+    const findUser = await UserModel.findOne({ email });
+    if (findUser) {
+      console.log(findUser);
+      return res
+        .status(404)
+        .send({ message: "User already exists with the same email" });
+    }
+
+    const hashPassword = btoa(password);
+    const user = new UserModel({
+      email,
+      password: hashPassword,
+      firstName,
+      lastName,
+    });
+
+    await user.save();
+    console.log(user);
+    return res.status(201).send({ message: "Successfully created user" });
+  } catch (error) {
+    return res.status(500).send({ message: error.message });
+  }
 });
 userRouter.get("/details", userAuth, async (req, res) => {
-    // console.log(req.body)
-    try {
-        const {userId} = req.body
-        const findUser = await UserModel.findOne({userId})
-        console.log(findUser)
-        if(!findUser) return res.status(404).send({message : "no user found"})
-        return res.status(201).send({message : "user found", data : findUser})
-    } catch (error) {
-        return res.status(404).send({message : error})        
-    }  
-})
+  try {
+    const { userId } = req.body;
+    if (!userId) return res.status(400).send({ message: "userId is required" });
+    const findUser = await UserModel.findOne({ _id: userId }).select(
+      "-password -__v"
+    );
+    if (!findUser) return res.status(404).send({ message: "No user found" });
 
-userRouter.get("/course/view", userAuth, async (req, res) => {
-    
-    try {
-        // const {userId} = req.body
-        const findPurchaseCourse = await PurchaseModel.find({userId})
-        const findCourse = await CourseModel.find().populate('creatorId', 'firstName lastName email');
-        if(findCourse.length > 0) return res.status(201).send({message : "success", data : findCourse})
-        return res.status(404).send({message : "no course found"})
-    } catch (error) {
-        return res.status(404).send({message : error})        
-    }  
-})
-
-
-
-userRouter.get("/course/mycourse", userAuth, async (req, res) => {
-   try {
-    const {userId} = req.body
-    const findCourse = await PurchaseModel.find({userId}).populate('courseId', 'title description price imageUrl creatorId')
-
-    if(findCourse.length > 0) return res.status(201).send({message : "user courses", data : findCourse});
-    return res.status(404).send({message : "no courses available"});
-
-   } catch (error) {
-    return res.status(201).send({message :error});    
-   }    
+    return res.status(200).send({ message: "User found", data: findUser });
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ message: "Server error", error: error.message });
+  }
 });
 
+userRouter.get("/course/view", userAuth, async (req, res) => {
+  try {
+    const findCourse = await CourseModel.find().populate(
+      "creatorId",
+      "firstName secondName email"
+    );
+    if (findCourse.length > 0)
+      return res.status(201).send({ message: "success", data: findCourse });
+    return res.status(404).send({ message: "no data found" });
+  } catch (error) {
+    return res.status(404).send({ message: error });
+  }
+});
+
+userRouter.get("/course/mycourse", userAuth, async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const findCourse = await PurchaseModel.find({ userId }).populate(
+      "courseId",
+      "title description price imageUrl creatorId"
+    );
+
+    if (findCourse.length > 0)
+      return res
+        .status(201)
+        .send({ message: "user courses", data: findCourse });
+    return res.status(404).send({ message: "no courses available" });
+  } catch (error) {
+    return res.status(201).send({ message: error });
+  }
+});
 
 userRouter.post("/course/buy", userAuth, async (req, res) => {
-    try {
-        const {userId, courseId} = req.body
-        findCourse = await PurchaseModel.findOne({userId, courseId})
-        if(findCourse) return res.status(404).send({message : "course already purchased"});
-    
-     const myCourse = new PurchaseModel({
-        userId,
-        courseId
-     })
+  try {
+    const { userId, courseId } = req.body;
+    findCourse = await PurchaseModel.findOne({ userId, courseId });
+    if (findCourse)
+      return res.status(404).send({ message: "course already purchased" });
 
-     await myCourse.save();
-     return res.status(201).send({message : "course purchases successful"}); 
+    const myCourse = new PurchaseModel({
+      userId,
+      courseId,
+    });
 
-    } catch (error) {
-     return res.status(201).send({message :error});    
-    }    
- });
+    await myCourse.save();
+    return res.status(201).send({ message: "course purchases successful" });
+  } catch (error) {
+    return res.status(201).send({ message: error });
+  }
+});
 
- 
-
-module.exports = { userRouter}
+module.exports = { userRouter };
